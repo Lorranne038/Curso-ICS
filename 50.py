0d51041a634231969d821932a2a97ec1ddf9fd1b
𝐋𝐨𝐫𝐫𝐚𝐧𝐧𝐞𝟎𝟑𝟖 <161883125+Lorranne038@users.noreply.github.com>
2024-06-01 08:16:39 -0300

    Add files via upload

---
+++ 50.py
@@ -0,0 +1,14 @@
+lista_de_entradaista_de_entrada =  [1, 2, 3, 4, 5]
+
+def troca_valores(reverse):
+    valor = reverse
+Res = troca_valores[1,2,3,4,5]
+
+for lista_de_entrada in Res:
+    print(Res)
+
+
+    
+
+
+
