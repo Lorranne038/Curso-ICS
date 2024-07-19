1ce7a044983c0c9646d67e750a323a2cbe8abf99
𝐋𝐨𝐫𝐫𝐚𝐧𝐧𝐞𝟎𝟑𝟖 <161883125+Lorranne038@users.noreply.github.com>
2024-07-19 09:29:09 -0300

    Add files via upload

---
+++ 51-60/desafio.py
@@ -0,0 +1,12 @@
+lista = [1,2,3,4,5,6,]
+
+def soma(resultado):
+    valor = 0 
+    for numero in resultado:
+        valor +=  numero
+
+    print(valor)
+soma(lista)
+
+
+ 
---
+++ 51-60/desafio_2.py
@@ -0,0 +1,12 @@
+palavra = ['Pirapora','Montes Claros','BH']
+
+def  ultima_letra(cidade):
+    
+    for palavra in cidade:
+
+        if palavra[-1] == 'H':
+            print(palavra)
+ultima_letra(palavra)
+
+
+
